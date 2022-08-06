3d6e9dec68d24bbe232f2b176ac2e97cf97c2df9
이민형 <leemihzy@gmail.com>
2022-08-06 21:01:43 +0900

    feat: add features - memo edit - set resize - memo position

---
+++ 302-sticker-memo/src/App.js
@@ -0,0 +1,48 @@
+import { observer } from 'mobx-react';
+import Memo from './Memo/Memo';
+import AddIcon from '@mui/icons-material/Add';
+import { useCallback } from 'react';
+
+function App({ store }) {
+  const AddMemo = useCallback(() => store.addMemo(), [store]);
+  const Edit = useCallback(
+    (id, content) => store.editMemo(id, content),
+    [store]
+  );
+  const SetWidthHeight = useCallback(
+    (id, width, height) => store.setWidthHeight(id, width, height),
+    [store]
+  );
+  const SetPosition = useCallback(
+    (id, x, y) => store.setPosition(id, x, y),
+    [store]
+  );
+
+  return (
+    <div className='App'>
+      {store.memos.map((memo) => (
+        <Memo
+          key={memo.id}
+          item={memo}
+          Edit={Edit}
+          SetWidthHeight={SetWidthHeight}
+          setPosition={SetPosition}
+        />
+      ))}
+      <AddIcon
+        style={{
+          float: 'right',
+          backgroundColor: '#e4e4e4',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          fontSize: '30px',
+          border: '1px solid #000',
+        }}
+        onClick={AddMemo}
+      />
+    </div>
+  );
+}
+
+// *observer 속성들이 변화가 일어나면 변화가 되는 곳을 바라보고 업데이트를 해줌
+export default observer(App);
---
+++ 302-sticker-memo/src/Memo/Memo.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Draggable from '@leemihzy/draggable';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+import CloseIcon from '@mui/icons-material/Close';
+import './Memo.scss';
+import {
+  useRef,
+  useMemo,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+} from 'react';
+import { debounce } from 'underscore';
+import { observer } from 'mobx-react';
+
+function Memo({ item, Delete, Edit, SetPosition, SetWidthHeight }) {
+  console.log(item);
+  const handleRef = useRef(null);
+  const memoContainer = useRef(null);
+  const onChangeMemo = useMemo(
+    () => debounce((e) => Edit(item.id, e.target.value), 500),
+    [item.id, Edit]
+  );
+
+  // *메모가 삭제된 후 debounce()가 일어나지 않게 처리
+  useEffect(() => {
+    return () => {
+      onChangeMemo.cancel();
+    };
+  }, [onChangeMemo]);
+
+  // *entry 옵저버 여러개
+  const onChangeSize = useMemo(
+    () =>
+      debounce((entry) => {
+        const { width, height } = entry[0].contentRect;
+        SetWidthHeight(item.id, width, height);
+      }, 500),
+    [item.id, SetWidthHeight]
+  );
+
+  useLayoutEffect(() => {
+    let RO = new ResizeObserver(onChangeSize);
+    RO.observe(memoContainer.current);
+    return () => {
+      RO.disconnect();
+      RO = null;
+    };
+  });
+
+  const onChangePosition = useCallback(
+    (x, y) => {
+      SetPosition(item.id, x, y);
+    },
+    [item.id, SetPosition]
+  );
+
+  return (
+    <Draggable handleRef={handleRef} x={0} y={0} onMove={onChangePosition}>
+      <div
+        className='memo-container'
+        style={{ width: `${250}px`, height: `${300}px` }}
+        ref={memoContainer}
+      >
+        <div className='menu'>
+          <DragHandleIcon
+            ref={handleRef}
+            sx={{ cursor: 'move', fontSize: '25px' }}
+          />
+          <CloseIcon
+            sx={{ cursor: 'pointer', fontSize: '25px', float: 'right' }}
+          />
+        </div>
+        <textarea
+          className='memo-text-area'
+          defaultValue={item.content}
+          name='txt'
+          placeholder='Enter memo here'
+          onChange={onChangeMemo}
+        ></textarea>
+      </div>
+    </Draggable>
+  );
+}
+
+export default observer(Memo);
